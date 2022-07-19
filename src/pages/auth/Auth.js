1ab6c5fa4bf7e40dec00f6c8c03fff3b656a86fb
Manue1ca <zavyazkin0302@gmail.com>
2022-07-19 21:10:30 +0900

    add auth login screen

--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -5,16 +5,36 @@ import Store from "../../store/Store";
 
 const Auth = observer(() =>{
     return (
-        <div className='auth'>
-            <div className='authForm'>
-                <img src={require("../../images/auth.png")} alt=""/>
-                <h2>Мы - твоя лучшая кулинарная книга!</h2>
-                <h3>Готовь из того, что найдешь на кухне - мы поможем тебе с выбором.</h3>
+        <>
+            <div className='auth authMain'>
+                <div className='authForm'>
+                    <img src={require("../../images/auth.png")} alt=""/>
+                    <h2>Мы - твоя лучшая кулинарная книга!</h2>
+                    <h3>Готовь из того, что найдешь на кухне - мы поможем тебе с выбором.</h3>
 
-                <button id='registrationBut' onClick={() => Store.logIn()}>Регистрация</button>
-                <button id='logIn'>Авторизоваться</button>
+                    <button id='registrationBut' onClick={() => Store.logIn()}>Регистрация</button>
+                    <button id='logIn'>Авторизоваться</button>
+                </div>
             </div>
-        </div>
+            <div className='auth authLogin'>
+                <div className='authForm'>
+                    <img src={require("../../images/Blobs.png")} alt=""/>
+                    <div className='authContainer'>
+                        <div className='authContainerDots'>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div className='formAuth'>
+                            <div><img src={require('../../images/Envelope.png')} alt=""/><input  type="text" placeholder='E-mail'/></div>
+                            <div><img src={require('../../images/Key.png')} alt=""/><input type="text" placeholder='Пароль'/></div>
+                            <button id='registrationBut' onClick={() => Store.logIn()}>Войти</button>
+                            <h3>Нет аккаунта? Зарегистрируйся!</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 })
 
